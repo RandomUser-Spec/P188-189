98238fe3bf3001e1f64cd219b592c3d1b51506fd
RandomUser-Spec <76138622+RandomUser-Spec@users.noreply.github.com>
2022-08-02 19:58:13 -0500

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,95 @@
+import hashlib 
+from tkinter import *
+from firebase import firebase
+from tkinter import messagebox
+
+
+registration_window = Tk()
+registration_window.geometry("400x400")
+registration_window.config(bg = 'lightpink')
+
+firebase = firebase.FirebaseApplication("https://loginsignin188-default-rtdb.firebaseio.com/", None)
+login_username_entry = ''
+login_password_entry = ''
+
+def login(): 
+    print("login function")
+    global login_username_entry
+    global login_password_entry
+    username = login_username_entry.get()
+    password = login_password_entry.get()
+    encrypted_password = hashlib.md5(password.encode())
+    hexd_password = encrypted_password.hexdigest()
+    get_password = firebase.get('/', username)
+    if (get_password != None):
+        if (get_password == hexd_password):
+            messagebox.showinfo('Message', 'Successfully Logged In!')
+        else:
+            messagebox.showinfo('Message', 'Please Check Your Password.')
+    else:
+        messagebox.showinfo('Message', 'User Not Registered! \n Get Yourself Registered First To Log In')
+        
+        
+        
+    
+def register(): 
+    print("register function")
+    username = username_entry.get()
+    password = password_entry.get()
+    password_result = hashlib.md5(password.encode())
+    password_hexd = password_result.hexdigest()
+    print(password_hexd)
+    firebase.put('https://loginsignin188-default-rtdb.firebaseio.com/', username, password_hexd)
+    
+    
+def login_window():
+    global login_username_entry
+    global login_password_entry
+    registration_window.destroy()
+    login_window = Tk()
+    login_window.geometry("400x400")
+    login_window.config(bg = 'lightblue')
+    
+    log_heading_label = Label(login_window, text="Log In" , font = 'arial 18 bold')
+    log_heading_label.place(relx=0.5,rely=0.2, anchor=CENTER)
+    
+    login_username_label = Label(login_window, text="Username : " , font = 'arial 13')
+    login_username_label.place(relx=0.3,rely=0.4, anchor=CENTER)
+    
+    login_username_entry = Entry(login_window)
+    login_username_entry.place(relx=0.6,rely=0.4, anchor=CENTER)
+    
+    login_password_label = Label(login_window, text="Password : " , font = 'arial 13')
+    login_password_label.place(relx=0.3,rely=0.5, anchor=CENTER)
+    
+    login_password_entry = Entry(login_window)
+    login_password_entry.place(relx=0.6,rely=0.5, anchor=CENTER)
+    
+    btn_login = Button(login_window, text="Log In" , font = 'arial 13 bold' , command=login, relief=FLAT)
+    btn_login.place(relx=0.5,rely=0.65, anchor=CENTER)
+    
+    login_window.mainloop()
+    
+    
+heading_label = Label(registration_window, text="Register" , font = 'arial 18 bold')
+heading_label.place(relx=0.5,rely=0.2, anchor=CENTER)
+
+username_label = Label(registration_window, text="Username : " , font = 'arial 13')
+username_label.place(relx=0.3,rely=0.4, anchor=CENTER)
+
+username_entry = Entry(registration_window)
+username_entry.place(relx=0.6,rely=0.4, anchor=CENTER)
+
+password_label = Label(registration_window, text="Password :  " , font = 'arial 13')
+password_label.place(relx=0.3,rely=0.5, anchor=CENTER)
+
+password_entry = Entry(registration_window)
+password_entry.place(relx=0.6,rely=0.5, anchor=CENTER)
+
+btn_reg = Button(registration_window, text="Sign Up" , font = 'arial 13 bold' ,command=register, relief=FLAT, padx=10)
+btn_reg.place(relx=0.5,rely=0.75, anchor=CENTER)
+
+btn_login_window = Button(registration_window, text="Log In" , font = 'arial 10 bold' ,  command=login_window, relief=FLAT)
+
+btn_login_window.place(relx=0.9,rely=0.06, anchor=CENTER)
+registration_window.mainloop()
